ab1a93144ec9351f07aab25f34b70c1607b01a35
Jacob Doar <jjdoar@gmail.com>
2023-04-25 21:59:02 -0700

    Add Motion visualization. Performance improvements needed

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import logging
 import sys
 
 from app import VideoVisualizerApp
-from visualization_mode import Original, Grayscale, Difference
+from visualization_mode import Motion, Original, Grayscale, Difference, Motion
 
 from PyQt5.QtWidgets import QApplication
 
@@ -10,7 +10,7 @@ from PyQt5.QtWidgets import QApplication
 def main():
     logging.basicConfig(level=logging.DEBUG)
     app = QApplication(sys.argv)
-    window = VideoVisualizerApp([Original(), Grayscale(), Difference()])
+    window = VideoVisualizerApp([Original(), Grayscale(), Difference(), Motion()])
     window.show()
     sys.exit(app.exec_())
 
--- src/visualization_mode.py
+++ src/visualization_mode.py
@@ -1,5 +1,7 @@
-from typing import Protocol
 import cv2
+import numpy as np
+
+from typing import Protocol
 
 
 class Mode(Protocol):
@@ -46,8 +48,39 @@ class Difference(Mode):
             self.prev_frame = frame
             return None
 
-        frame_copy = frame.copy()
-        frame = cv2.absdiff(frame, self.prev_frame)
-        self.prev_frame = frame_copy
+        frame_diff = cv2.absdiff(frame, self.prev_frame)
+        self.prev_frame = frame
 
-        return frame
+        return frame_diff
+
+
+class Motion(Mode):
+    def __init__(self):
+        self.prev_gray = None
+
+    def apply(self, frame):
+        # VERY POOR PERFORMANCE
+        if frame is None:
+            self.prev_gray = None
+            return
+
+        if self.prev_gray is None:
+            self.prev_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            return None
+
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame_motion = cv2.calcOpticalFlowFarneback(
+            self.prev_gray, frame_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0
+        )
+        self.prev_gray = frame_gray
+
+        angle = np.arctan2(frame_motion[..., 1], frame_motion[..., 0])
+        magnitude = np.sqrt(frame_motion[..., 0] ** 2 + frame_motion[..., 1] ** 2)
+        normalized_magnitude = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX)
+
+        hsv = np.zeros((frame_gray.shape[0], frame_gray.shape[1], 3), dtype=np.float32)
+        hsv[..., 0] = angle * 180 / np.pi / 2
+        hsv[..., 1] = 255
+        hsv[..., 2] = normalized_magnitude
+
+        return cv2.cvtColor(hsv.astype(np.uint8), cv2.COLOR_HSV2BGR)
